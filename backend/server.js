9f60a30a411133970e124065272e93f3aa0ddb57
Ahmad Bilal <ahmadbilal71al@gmail.com>
2026-08-24 20:59:18 +0500

    solve cors issues

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,17 @@ dotenv.config();
 import db from "./src/config/db.js";
 import authRouter from "./src/routers/authRouter.js";
 import errorHandler from "./src/middlewares/errorHandler.js";
+import cors from "cors";
 
 const app = express();
+
+const corsOrigin = {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}
+
+app.use(cors())
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
